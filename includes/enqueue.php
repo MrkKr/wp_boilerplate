db2097d25415723faedb9416e86bf147bc90bf75
Filip Wachowiak <filipw01@interia.eu>
2019-05-09 10:47:09 +0200

    HMR not working (issue on Gihub opened)

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -4,6 +4,5 @@
 function load_css_js() {
 	wp_enqueue_style('main-css', get_template_directory_uri() . '/dist/style.css', false, null);
     wp_enqueue_script('main-scripts', get_template_directory_uri() . '/dist/script.js', array('jquery'), null, true);
-    wp_enqueue_script('ajax-scripts', get_template_directory_uri() . '/dist/ajax.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'load_css_js', 100);
