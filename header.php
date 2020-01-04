b66a6bb099bd2154e49196a0c51ca921c499a4c1
Filip Wachowiak <filipw01@interia.eu>
2020-01-04 18:01:10 +0100

    Update dependencies, add noindex on *.stronazen.pl

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?= strpos($_SERVER['SERVER_NAME'], 'stronazen.pl')?'<meta name="robots" content="noindex">':''?>
     <?php wp_head(); ?>
 
     <?php if ( is_admin_bar_showing() ) {?>
